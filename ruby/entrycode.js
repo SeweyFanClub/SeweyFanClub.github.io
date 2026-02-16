80c9989dbf38d904fc7291fe8a9706ebd4214ad8
shoesareme <77761814+shoesareme@users.noreply.github.com>
2026-02-16 12:57:50 -0500

    debe

--- ruby/entrycode.js
+++ ruby/entrycode.js
@@ -1,4 +1,5 @@
 function sumbit() {
+  console.log("hi");
   const ec = document.getElementsByName("ec").value; 
   if (ec == "CHILLGUY") {
     window.location = "https://seweyfanclub.github.io/ruby/emp5/";
